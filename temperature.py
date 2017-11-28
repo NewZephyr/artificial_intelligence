3567502f28584900ab72aaacc8910b351965cfc9
NewZephyr <27190160+NewZephyr@users.noreply.github.com>
2017-11-28 19:38:51 +0200

    Add files via upload

--- temperature.py
+++ temperature.py
@@ -4,37 +4,20 @@ use("TkAgg")
 import consts
 from matplotlib import pyplot as plt
 
-
-
 X = np.array([400, 900, 390, 1000, 550])
 sorted_x = np.sort(X)[:consts.Consts.STOCH_TOP_SCORES_TO_CONSIDER]
-best_n = list(map(lambda x: x if x in sorted_x else 0, X))
 
 
-# def p(x, t):
-#     return (0 if x == 0 else np.power(x, -1 / t) / (np.sum(np.power(sorted_x, -1 / t))))
-def p(x, t):
-    if x == 0:
-        return 0
-    else:
-        return np.power(x, -1 / t) / (np.sum(np.power(sorted_x, -1 / t)))
+def p(x_i, t):
+    return 0 if x_i == 0 else (x_i**(-1 / t) / (np.sum(np.power(sorted_x, -1 / t))))
 
+plt.title("Probability as a function of the temperature")
 plt.xlabel("T")
 plt.ylabel("P")
-plt.title("Probability as a function of the temperature")
-t = np.arange(0, 5, 0.05)
-y = np.array(best_n)
-plt.plot(t,y)
-
+plt.grid(True)
+ax = plt.subplot(111)
+for x in X:
+    p_list = [p(x, t) for t in np.arange(0.01, 5, 0.05)]
+    ax.plot(np.arange(0.01, 5, 0.05), p_list, label=x)
+ax.legend(bbox_to_anchor=(1.1, 1.05))
 plt.show()
-
-
-
-
-# def p(t,X):
-#     sorted_x = np.sort(X)[:consts.Consts.STOCH_TOP_SCORES_TO_CONSIDER]
-#     best_n = list(map(lambda x: x if x in sorted_x else 0, X))
-#     result = []
-#     for i in best_n:
-#         result += [0] if i == 0 else [np.power(i, -1 / t) / (np.sum(np.power(sorted_x, -1 / t)))]
-#     return result
